4b90df8d5d8661632df18ff638a93b5175992d1e
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2013-07-14 17:27:36 +0900

    Site updated at 2013-07-14 08:27:36 UTC

--- javascripts/github.js
+++ javascripts/github.js
@@ -1,1 +1,32 @@
-var github=function(){function t(t){return $("<div/>").text(t).html()}function r(r,a){var n=0,e="",o=$(r)[0];for(n=0;n<a.length;n++)e+='<li><a href="'+a[n].html_url+'">'+a[n].name+"</a><p>"+t(a[n].description||"")+"</p></li>";o.innerHTML=e}return{showRepos:function(t){$.ajax({url:"https://api.github.com/users/"+t.user+"/repos?sort=pushed&callback=?",dataType:"jsonp",error:function(){$(t.target+" li.loading").addClass("error").text("Error loading feed")},success:function(a){var n=[];if(a&&a.data){for(var e=0;e<a.data.length;e++)t.skip_forks&&a.data[e].fork||n.push(a.data[e]);t.count&&n.splice(t.count),r(t.target,n)}}})}}}();
+var github = (function(){
+  function escapeHtml(str) {
+    return $('<div/>').text(str).html();
+  }
+  function render(target, repos){
+    var i = 0, fragment = '', t = $(target)[0];
+
+    for(i = 0; i < repos.length; i++) {
+      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+escapeHtml(repos[i].description||'')+'</p></li>';
+    }
+    t.innerHTML = fragment;
+  }
+  return {
+    showRepos: function(options){
+      $.ajax({
+          url: "https://api.github.com/users/"+options.user+"/repos?sort=pushed&callback=?"
+        , dataType: 'jsonp'
+        , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
+        , success: function(data) {
+          var repos = [];
+          if (!data || !data.data) { return; }
+          for (var i = 0; i < data.data.length; i++) {
+            if (options.skip_forks && data.data[i].fork) { continue; }
+            repos.push(data.data[i]);
+          }
+          if (options.count) { repos.splice(options.count); }
+          render(options.target, repos);
+        }
+      });
+    }
+  };
+})();
